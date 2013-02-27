cbcc48ce247ffe27efcd39e94249386dac31f80d
Henter <henter@henter.me>
2013-02-27 22:53:19 +0800

    change init.sql temporarily for bug.

--- init.sql
+++ init.sql
@@ -1,44 +1,100 @@
+/*
+Navicat MySQL Data Transfer
 
-#创建articles表，存储blog文章
-CREATE TABLE articles(
-	aid				INT		AUTO_INCREMENT PRIMARY KEY	,
-	title 			VARCHAR(20) ,
-	content 		TEXT		,	
-	posttime		DATETIME	,
-	classid			INT			,
-	tags			VARCHAR(50)	,
-	classname		VARCHAR(50)	,
-	summary			VARCHAR(50)	,
-	pv				INT			,
-	commentnumber 	INT			);
+Source Server         : localhost
+Source Server Version : 50128
+Source Host           : localhost:3306
+Source Database       : app_chouyangbox
+
+Target Server Type    : MYSQL
+Target Server Version : 50128
+File Encoding         : 65001
+
+Date: 2013-02-27 22:50:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `admins`
+-- ----------------------------
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE `admins` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `userpass` varchar(100) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of admins
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `articles`
+-- ----------------------------
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) DEFAULT NULL,
+  `content` text,
+  `posttime` datetime DEFAULT NULL,
+  `classid` int(11) DEFAULT NULL,
+  `tags` varchar(50) DEFAULT NULL,
+  `classname` varchar(50) DEFAULT NULL,
+  `summary` varchar(50) DEFAULT NULL,
+  `pv` int(11) DEFAULT NULL,
+  `commentnumber` int(11) DEFAULT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of articles
+-- ----------------------------
+INSERT INTO `articles` VALUES ('1', 'test article', 'test content', '2013-02-27 22:38:39', '1', 'fuck python', 'testclass', 'test summary', '2432', '4');
+
+-- ----------------------------
+-- Table structure for `classes`
+-- ----------------------------
+DROP TABLE IF EXISTS `classes`;
+CREATE TABLE `classes` (
+  `classid` int(11) NOT NULL AUTO_INCREMENT,
+  `classname` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`classid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of classes
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `comments`
+-- ----------------------------
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `aid` int(11) DEFAULT NULL,
+  `posttime` datetime DEFAULT NULL,
+  `content` text,
+  `email` varchar(50) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comments
+-- ----------------------------
+INSERT INTO `comments` VALUES ('1', '1', '2013-02-27 22:40:14', 'test comment', 'henter@henter.me', 'henter');
 
 
-
-CREATE TABLE comments(
-	
-	cid				INT 	AUTO_INCREMENT	PRIMARY KEY	,
-	aid				INT 		,
-	posttime		DATETIME	,
-	content			TEXT		,
-	email			VARCHAR(50)	,
-	username		VARCHAR(20)	);
-
-
-CREATE TABLE classes(
-	classid   INT AUTO_INCREMENT PRIMARY KEY,
-	classname VARCHAR(20) );
+-- ----------------------------
+-- Table structure for `sessions`
+-- ----------------------------
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `session_id` char(128) NOT NULL,
+  `atime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `data` text,
+  UNIQUE KEY `session_id` (`session_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE  `app_chouyangbox`.`admins` (
-`uid` INT NOT NULL AUTO_INCREMENT ,
-`username` VARCHAR( 20 ) NOT NULL ,
-`userpass` VARCHAR( 100 ) NOT NULL ,
-PRIMARY KEY (  `uid` )
-) ;
-
-
-CREATE TABLE  sessions ( 
-session_id char(128) UNIQUE NOT NULL, 
-atime timestamp NOT NULL default current_timestamp, 
-data text 
-); 
